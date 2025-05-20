14ae9f795405637954e1bafcbb1bd10cdeeb74ac
JacksonMessa <jacksonadrianomessa@gmail.com>
2025-05-20 16:32:48 -0300

    fix: recreate tables changing brackets for parentheses in VARCHAR

--- NewsAPI/src/main/resources/db/migration/V1__create-user-table.sql
+++ NewsAPI/src/main/resources/db/migration/V1__create-user-table.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE users(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    username VARCHAR[255] NOT NULL,
-    password VARCHAR[255] NOT NULL,
-    role VARCHAR[10]
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(10)
 );
--- NewsAPI/src/main/resources/db/migration/V2__create-news-table.sql
+++ NewsAPI/src/main/resources/db/migration/V2__create-news-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE news(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    title VARCHAR[255] NOT NULL,
+    title VARCHAR(255) NOT NULL,
     body TEXT NOT NULL,
     writer_id UUID NOT NULL,
     FOREIGN KEY (writer_id) REFERENCES users(id) ON DELETE CASCADE
